8a86c3bebe1e16f11315ae3104d5f8e4cc7e690a
Boundless <72645575+Starmoon0@users.noreply.github.com>
2021-09-07 16:31:28 +0800

    修复bug
    替换Timer，修复延迟问题，待测试……

--- src/com/starmoon/util/TimerUtils.java
+++ src/com/starmoon/util/TimerUtils.java
@@ -1,34 +1,71 @@
 package com.starmoon.util;
 
 import java.util.Calendar;
-import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
+
+import static java.util.Calendar.DAY_OF_MONTH;
+import static java.util.Calendar.HOUR_OF_DAY;
+import static java.util.Calendar.MINUTE;
+import static java.util.Calendar.SECOND;
+import java.util.HashMap;
+
 
 // 每日定点时间执行
-public class TimerUtils
-{
-    private static final long PERIOD_DAY = 24 * 60 * 60 * 1000;
-    public static Timer startTimer(TimerTask task, int hour, int minute, int second)
-    {
-        Calendar calendar=Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, hour);
-        calendar.set(Calendar.MINUTE, minute);
-        calendar.set(Calendar.SECOND, second);
-        
-        Date date=calendar.getTime();
-        if(date.before(new Date())){
-            date=addDay(date, 1);
+public class TimerUtils {
+    public static HashMap<String, a> mTimers=new HashMap<>();
+    public static a startTimer(Runnable run, String name, int hour, int minute, int second) {
+        a a=new a(run, hour, minute, second);
+        a a1 = mTimers.getOrDefault(name, a);
+        if (a1 != a || !a.isRunning()) {
+            mTimers.put(name, a);
+            new Thread(a).start();
         }
-        
-        Timer timer=new Timer();
-        timer.schedule(task, date, PERIOD_DAY);
-        return timer;
+        return a;
     }
-    public static Date addDay(Date date, int num) {
-        Calendar startDT = Calendar.getInstance();
-        startDT.setTime(date);
-        startDT.add(Calendar.DAY_OF_MONTH, num);
-        return startDT.getTime();
+    public static class a implements Runnable {
+        private boolean isRun;
+        private int date;
+        private final int hour;
+        private final int minute;
+        private final int second;
+
+        private final Runnable runnable;
+        private boolean isRunnable=true;
+
+        a(Runnable run, int h, int m, int s) {
+            date = -1;
+            runnable = run;
+            hour = h;
+            minute = m;
+            second = s;
+        }
+
+
+        @Override
+        public void run() {
+            while (isRunnable) {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {}
+                Calendar ca=Calendar.getInstance();
+                if (isRun) {
+                    if (date == ca.get(DAY_OF_MONTH)) {
+                        if (second != ca.get(SECOND)) {
+                            isRun = false;
+                        }
+                        continue;
+                    }
+                } else if (date != ca.get(DAY_OF_MONTH) && second == ca.get(SECOND) && minute == ca.get(MINUTE) && hour == ca.get(HOUR_OF_DAY)) {
+                    isRun = true;
+                    date = ca.get(DAY_OF_MONTH);
+                    runnable.run();
+                }
+            }
+        }
+        public void cancel() {
+            isRunnable = false;
+        }
+        public boolean isRunning() {
+            return isRunnable;
+        }
     }
 }
